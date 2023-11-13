6b407ba2c162c9a3c7c79a9885e09b435e5f0282
raffib04 <rafaelbiedron2026@u.northwestern.edu>
2023-11-13 16:48:46 -0600

    Test

--- raisow24/app/dashboard/page.tsx
+++ raisow24/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
     return (
         <p>
-            <div className='px-[20%] py-[10%]'></div>
+            <div className='px-[20%] py-[10%]'>
+                <h1 className='text-4xl font-bold'>Page</h1>
+            </div>
         </p>
     );
 }
