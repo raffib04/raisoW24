bcfbaf69dfeb29042a4c05cf6c56baa43653a5ff
raffib04 <rafaelbiedron2026@u.northwestern.edu>
2023-11-13 16:33:22 -0600

    init stuff

---
+++ raisow24/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <p>
+            <div className='px-[20%] py-[10%]'></div>
+        </p>
+    );
+}
---
+++ raisow24/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../ui/login-form";
+import Search from "../ui/search";
+
+export default function Page() {
+    return (
+        <p>
+            <div className='px-[20%] py-[10%]'>
+                <LoginForm />
+            </div>
+        </p>
+    );
+}
